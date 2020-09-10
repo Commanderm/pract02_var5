a487ccadbb013d06f00e39f78eb4a4de8d2a0665
Волков Михаил <commanderm@yandex.ru>
2020-09-10 17:20:06 +0300

    Initial commit

---
+++ src/ru/mirea/lab01var5/Fact.java
@@ -0,0 +1,25 @@
+package ru.mirea.lab01var5;
+
+import java.lang.*;
+
+public class Fact {
+
+    private int n;
+    double fact = 1;
+
+    public Fact(int n){
+        this.n = n;
+    }
+
+    public double calculateFactorial() {
+        while (n != 0) {
+            fact *= n;
+            n--;
+        }
+        return fact;
+    }
+
+    public String toString() {
+        return "Factorial number's is "+ fact;
+    }
+}
---
+++ src/ru/mirea/lab01var5/TestFact.java
@@ -0,0 +1,17 @@
+package ru.mirea.lab01var5;
+
+import java.lang.*;
+
+public class TestFact {
+
+    public static void main(String[] args){
+
+        Fact f1 = new Fact(10);
+
+        f1.calculateFactorial();
+
+        System.out.println(f1);
+
+    }
+
+}
